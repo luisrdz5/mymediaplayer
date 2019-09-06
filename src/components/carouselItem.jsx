db679c2aa6f30f6842a879176ac9eb4d329138eb
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2019-09-05 19:43:39 -0500

    Se ha empezado a desarrollar nuestro reproductor de video

--- src/components/carouselItem.jsx
+++ src/components/carouselItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { setFavorite, deleteFavorite } from '../actions';
 import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
@@ -22,7 +23,13 @@ const CarouselItem = (props) => {
       <img className='carousel-item__img' src={cover} alt={title} />
       <div className='carousel-item__details'>
         <div>
-          <img className='carousel-item__details--img' src={ playIcon } alt='play' />
+          <Link to={`/player/${id}`}>
+            <img 
+              className='carousel-item__details--img' 
+              src={ playIcon } 
+              alt='play' 
+            />
+          </Link>
           {isList ? 
             <img 
               className='carousel-item__details--img' 
