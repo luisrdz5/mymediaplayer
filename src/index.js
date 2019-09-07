aef8c417c8aa1b14f5c717b407007b10c0b1338c
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2019-09-06 20:11:16 -0500

    Se ha añadido a nuestro proyecto la conexion con redux web tools para poder debuggear nuestra aplicación

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import reducer from './reducers';
 import App from './routes/App';
 
@@ -172,7 +172,8 @@ const initialState = {
     ]
 }
 
-const store = createStore(reducer, initialState);
+const composeEnharncers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnharncers());
 
 ReactDOM.render(
   <Provider store={store}>
