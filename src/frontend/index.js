95a4ca489202c1daddaa1225b66d43b02ef6b10e
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2019-10-04 12:53:39 -0500

    added hidrate to make our project more efficient

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose } from 'redux';
 import { Router } from 'react-router';
 import { createBrowserHistory } from 'history';
 import reducer from './reducers';
 import App from './routes/App';
-import initialState from './initialState';
 
-const composeEnhacers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, initialState, composeEnhacers());
-const history = createBrowserHistory();
+if (typeof window !== 'undefined') {
+  const composeEnhacers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const preloadedState = window.__PRELOADED_STATE__;
+  const store = createStore(reducer, preloadedState, composeEnhacers());
+  const history = createBrowserHistory();
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('app'),
-);
+  hydrate(
+    <Provider store={store}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>,
+    document.getElementById('app'),
+  );
+}
--- src/server/render/index.js
+++ src/server/render/index.js
@@ -1,5 +1,5 @@
-const render = (html) => {
-  return (`
+const render = (html, preloadedState) => {
+  return `
     <!DOCTYPE html>
         <html lang="es">
             <head>
@@ -8,11 +8,16 @@ const render = (html) => {
             </head>
             <body>
                 <div id="app"> ${html} </div>
+                <script>
+                // WARNING: See the following for security issues around embedding JSON in HTML:
+                // http://redux.js.org/recipes/ServerRendering.html#security-considerations
+                window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')};
+              </script>
                 <script src ="assets/app.js" type="text/javascript" > </script>
                 <script src ="assets/vendor.js" type="text/javascript" > </script>
             </body>
         </html>
-    `);
+    `;
 };
 
 export default render;
