01a1ddb036cfbcb6c3a751de81ae2388f7060bcf
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2019-10-04 13:55:03 -0500

    instaled and configured our server to minify our code using terser-webpack-plugin

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -8,8 +8,9 @@ import reducer from './reducers';
 import App from './routes/App';
 
 if (typeof window !== 'undefined') {
-  const composeEnhacers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  let composeEnhacers;
+  if (process.env.NODE_ENV === 'production') composeEnhacers = compose;
+  else composeEnhacers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const preloadedState = window.__PRELOADED_STATE__;
   const store = createStore(reducer, preloadedState, composeEnhacers());
   const history = createBrowserHistory();
