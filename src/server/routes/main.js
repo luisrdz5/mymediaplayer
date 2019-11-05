6a190a3b9f0e7d4a472ef08000239c3274e5ef5f
luisrdz5 <luis@luisrodriguez.com>
2019-11-05 08:48:52 -0600

    fixed some bugs

--- src/server/routes/main.js
+++ src/server/routes/main.js
@@ -36,15 +36,18 @@ const main = async (req, res, next) => {
         headers: { Authorization: `Bearer ${token}` },
         method: 'get',
       });
-      console.log(`Aqui va el userMovies: ${userMovies.data.data}`);
-      console.log(`Aqui va el userMovies: ${movieList.data.data}`);
       movieList = movieList.data.data;
       userMovies = userMovies.data.data;
 
       initialState = {
         user,
         playing: {},
-        myList: userMovies.filter(movie => movie._id === id),
+        myList: userMovies.filter(info => {
+          console.log(info.movieId);
+          console.log(movieList.filter(movie => movie.id === info.movieId));
+          return (movieList.filter(movie => movie.id === info.movieId));
+        }),
+        //myList: movieList.filter(movie => movie.contentRating === 'PG' && movie.id),
         trends: movieList.filter(movie => movie.contentRating === 'PG' && movie.id),
         originals: movieList.filter(movie => movie.contentRating === 'G' && movie.id),
       };
